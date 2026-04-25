ac59fecf3d2cfbe55ee3b9a7c9dc2a896e82244f
HabibaFarouk <habiba.mfaroukk@gmail.com>
2026-04-25 13:48:03 +0300

    Controllers

--- src/Controllers/CampusCareController.js
+++ src/Controllers/CampusCareController.js
@@ -1,1 +1,19 @@
 const prisma = require('../prismaClient');
+
+//1. Authentication & Authorization APIs
+
+//2. Issue Management APIs
+//2.1 For Community Members (CM)
+
+
+//2.2 For Facility Managers (FM)
+
+
+//2.3 For Workers (W)
+
+
+//3. Managerial & Admin APIs 
+//3.1 Facility Manager (Worker Management)
+
+
+//3.2 System Admin (User Management)
