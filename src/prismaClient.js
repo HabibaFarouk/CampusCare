5007777fa2dbce4c0a1487603deef1686d22cedd
Habiba Mohamed Farouk <habiba.mfaroukk@gmail.com>
2026-05-09 21:27:17 +0300

    Delete src directory

--- src/prismaClient.js
+++
@@ -1,8 +0,0 @@
-// src/prismaClient.js
-const { PrismaClient } = require('@prisma/client'); // [cite: 138]
-
-// Initialize a single instance of PrismaClient
-const prisma = new PrismaClient();
-
-// Export the instance to be used in controllers
-module.exports = prisma; 
